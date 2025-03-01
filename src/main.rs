ad8c73dd41b742480265b90bfb4d6a42e61a364a
indierusty <priyaayadav@gmail.com>
2025-03-01 19:05:44 +0530

    draw strokes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Boxxy")]
+async fn main() {
+    let mut strokes = Vec::new();
+
+    loop {
+        clear_background(WHITE);
+        if is_mouse_button_down(MouseButton::Left) {
+            strokes.push(vec2(mouse_position().0, mouse_position().1));
+        }
+        for s in &strokes {
+            draw_circle(s.x, s.y, 5., DARKGRAY);
+        }
+        next_frame().await;
+    }
 }
