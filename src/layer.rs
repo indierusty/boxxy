cffbf73fa4cf1814f407072df3eaf51ad2e6f287
indierusty <priyaayadav@gmail.com>
2025-03-21 12:19:32 +0530

    draw pixels and render layer as a texture

---
+++ src/layer.rs
@@ -0,0 +1,67 @@
+use macroquad::prelude::*;
+
+pub struct Layer {
+    pixels: Vec<Color>,
+    size: UVec2,
+    transform: Affine2,
+}
+
+impl Layer {
+    pub fn new(size: UVec2, position: Vec2) -> Self {
+        let length = (size.x * size.y) as usize;
+        let mut pixels = Vec::with_capacity(length);
+        pixels.resize(length, BLACK);
+        Self {
+            pixels,
+            size,
+            transform: Affine2::from_scale_angle_translation(vec2(10., 10.), 0., position),
+        }
+    }
+
+    pub fn draw(&mut self) {
+        let pixels: Vec<u8> = self
+            .pixels
+            .iter()
+            .flat_map(|c| [c.r, c.g, c.b, c.a])
+            .map(|v| (v * 255.) as u8)
+            .collect();
+
+        let texture = Texture2D::from_rgba8(self.size.x as u16, self.size.y as u16, &pixels);
+        let x = self.transform.translation.x;
+        let y = self.transform.translation.y;
+        let size = self.transform.matrix2 * vec2(self.size.x as f32, self.size.y as f32);
+
+        draw_texture_ex(
+            &texture,
+            x,
+            y,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(size),
+                ..Default::default()
+            },
+        );
+    }
+
+    pub fn pixel(&mut self, pos: Vec2) {
+        let index = (pos.y as u32 * self.size.x) + pos.x as u32;
+        let index = index as usize;
+        if index < self.pixels.len() {
+            self.pixels[index] = BLUE;
+        }
+    }
+}
+
+impl Layer {
+    pub fn screen_to_local(&self, point: Vec2) -> Vec2 {
+        self.transform.matrix2.inverse() * (point - self.transform.translation)
+    }
+
+    pub fn translate(&mut self, factor: Vec2) {
+        self.transform.translation += factor;
+    }
+
+    pub fn scale(&mut self, factor: Vec2) {
+        self.transform.matrix2 *= Mat2::from_scale_angle(factor, 0.);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use macroquad::prelude::*;
 
+mod layer;
+use layer::Layer;
+
 #[macroquad::main("Boxxy")]
 async fn main() {
-    let mut strokes = Vec::new();
+    let mut layer = Layer::new(uvec2(50, 50), vec2(100., 50.));
 
     loop {
         clear_background(WHITE);
+
+        let mut scale = Vec2::ONE;
+        if is_key_down(KeyCode::K) {
+            scale = vec2(1.01, 1.01);
+        } else if is_key_down(KeyCode::J) {
+            scale = vec2(0.99, 0.99);
+        }
+
+        let mut translation = Vec2::ZERO;
+        if is_key_down(KeyCode::Left) {
+            translation.x = -5.;
+        } else if is_key_down(KeyCode::Right) {
+            translation.x = 5.;
+        } else if is_key_down(KeyCode::Up) {
+            translation.y = -5.;
+        } else if is_key_down(KeyCode::Down) {
+            translation.y = 5.;
+        };
+
+        layer.translate(translation);
+        layer.scale(scale);
+
         if is_mouse_button_down(MouseButton::Left) {
-            strokes.push(vec2(mouse_position().0, mouse_position().1));
+            let point = layer.screen_to_local(vec2(mouse_position().0, mouse_position().1));
+            layer.pixel(point);
         }
-        for s in &strokes {
-            draw_circle(s.x, s.y, 5., DARKGRAY);
-        }
+        layer.draw();
         next_frame().await;
     }
 }
